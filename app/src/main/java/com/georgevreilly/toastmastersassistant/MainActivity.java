7da4f70b9479e7b859787024e9a8f8d1a7b5298f
George V. Reilly <george@cookbrite.com>
2016-04-03 16:20:00 -0700

    Remove no-longer-used code

--- app/src/main/java/com/georgevreilly/toastmastersassistant/MainActivity.java
+++ app/src/main/java/com/georgevreilly/toastmastersassistant/MainActivity.java
@@ -1,46 +1,23 @@
 package com.georgevreilly.toastmastersassistant;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-
-import java.util.ArrayList;
-
 import android.app.ExpandableListActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ExpandableListView;
 
+import java.util.ArrayList;
+
+
 public class MainActivity extends ExpandableListActivity {
-    private ArrayList<String> parentItems = new ArrayList<String>();
-    private ArrayList<Object> childItems = new ArrayList<Object>();
+    private ArrayList<String> parentItems = new ArrayList<>();
+    private ArrayList<Object> childItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-
         super.onCreate(savedInstanceState);
-
-        // this is not really  necessary as ExpandableListActivity contains an ExpandableList
-        //setContentView(R.layout.main);
 
         ExpandableListView expandableList = getExpandableListView(); // you can use (ExpandableListView) findViewById(R.id.list)
 
