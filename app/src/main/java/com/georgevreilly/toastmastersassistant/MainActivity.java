e4f67e31dacc2c51905e6e37271c3036bedbddec
George V. Reilly <george@cookbrite.com>
2016-04-03 15:37:45 -0700

    ExpandableListView example

--- app/src/main/java/com/georgevreilly/toastmastersassistant/MainActivity.java
+++ app/src/main/java/com/georgevreilly/toastmastersassistant/MainActivity.java
@@ -9,23 +9,54 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+import android.app.ExpandableListActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.ExpandableListView;
+
+public class MainActivity extends ExpandableListActivity {
+    private ArrayList<String> parentItems = new ArrayList<String>();
+    private ArrayList<Object> childItems = new ArrayList<Object>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        super.onCreate(savedInstanceState);
+
+        // this is not really  necessary as ExpandableListActivity contains an ExpandableList
+        //setContentView(R.layout.main);
+
+        ExpandableListView expandableList = getExpandableListView(); // you can use (ExpandableListView) findViewById(R.id.list)
+
+        expandableList.setDividerHeight(2);
+        expandableList.setGroupIndicator(null);
+        expandableList.setClickable(true);
+
+        setGroupParents();
+        setChildData();
+
+        MyExpandableAdapter adapter = new MyExpandableAdapter(parentItems, childItems);
+
+        adapter.setInflater((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE), this);
+        expandableList.setAdapter(adapter);
+        expandableList.setOnChildClickListener(this);
+
     }
 
     @Override
@@ -49,4 +80,47 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    public void setGroupParents() {
+        parentItems.add("Android");
+        parentItems.add("Core Java");
+        parentItems.add("Desktop Java");
+        parentItems.add("Enterprise Java");
+    }
+
+    public void setChildData() {
+
+        // Android
+        ArrayList<String> child = new ArrayList<String>();
+        child.add("Core");
+        child.add("Games");
+        childItems.add(child);
+
+        // Core Java
+        child = new ArrayList<String>();
+        child.add("Apache");
+        child.add("Applet");
+        child.add("AspectJ");
+        child.add("Beans");
+        child.add("Crypto");
+        childItems.add(child);
+
+        // Desktop Java
+        child = new ArrayList<String>();
+        child.add("Accessibility");
+        child.add("AWT");
+        child.add("ImageIO");
+        child.add("Print");
+        childItems.add(child);
+
+        // Enterprise Java
+        child = new ArrayList<String>();
+        child.add("EJB3");
+        child.add("GWT");
+        child.add("Hibernate");
+        child.add("JSP");
+        childItems.add(child);
+    }
+
 }
